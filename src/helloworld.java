40d05dea682dea6d70c09c68db29e3d540e9537a
timofonic <timofonic@gmail.com>
2017-10-29 23:24:09 +0100

    First commit of a very simple initial project about showing "Hello world!" in screen

---
+++ src/helloworld.java
@@ -0,0 +1,10 @@
+/**
+ * This very simple app shows the "Hello world!" phrase on the screen.
+ * @author timofonic
+ */
+
+public class helloworld { // Main class
+    public static void main(String[] args) { // Specifies the kind of class and calls the String..
+        System.out.println("Hello world!"); // Makes a call to println in System.out (explain it better)
+    }
+}
